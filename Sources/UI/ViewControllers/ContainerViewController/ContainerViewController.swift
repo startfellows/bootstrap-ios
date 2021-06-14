c9f01d2aa84a90eb7efa5a2db54487b9999a3309
Anton Spivak <oboupo@gmail.com>
2021-06-14 18:39:51 +0300

    Fix appearance updating

--- Sources/UI/ViewControllers/ContainerViewController/ContainerViewController.swift
+++ Sources/UI/ViewControllers/ContainerViewController/ContainerViewController.swift
@@ -10,14 +10,20 @@ public class ContainerViewController: UIViewController {
     
     public override var childForStatusBarStyle: UIViewController? { contentViewController }
     public override var childForHomeIndicatorAutoHidden: UIViewController? { contentViewController }
-    public override var childForScreenEdgesDeferringSystemGestures: UIViewController? { contentViewController }
     public override var childForStatusBarHidden: UIViewController? { contentViewController }
+    public override var childViewControllerForPointerLock: UIViewController? { contentViewController }
+    public override var childForScreenEdgesDeferringSystemGestures: UIViewController? { contentViewController }
     
     private var containerView: ContainerView { view as! ContainerView }
     
     public override func loadView() {
         let view = ContainerView()
         self.view = view
+    }
+    
+    public override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        updateAppearance(animated: true)
     }
     
     public func setContentViewController(_ viewController: UIViewController?, animationOptions options: UIView.AnimationOptions? = nil) {
@@ -45,20 +51,17 @@ public class ContainerViewController: UIViewController {
                 containerView.setContentView(contentViewController.view, removePreviousAutomatically: false)
             }
             
+            contentViewController = viewController
             UIView.animate(withDuration: 0.62, delay: 0.0, options: options!, animations: {
                 viewController?.view.alpha = 1
                 previousContentViewController?.view.alpha = 0
                 
-                self.setNeedsStatusBarAppearanceUpdate()
-                self.setNeedsUpdateOfHomeIndicatorAutoHidden()
-                self.setNeedsUpdateOfScreenEdgesDeferringSystemGestures()
+                self.updateAppearance(animated: false)
             }, completion: { finished in
                 viewController?.didMove(toParent: self)
                 
                 previousContentViewController?.view.removeFromSuperview()
                 previousContentViewController?.removeFromParent()
-                
-                self.contentViewController = viewController
             })
         } else {
             contentViewController = viewController
@@ -70,9 +73,24 @@ public class ContainerViewController: UIViewController {
                 contentViewController.didMove(toParent: self)
             }
             
-            setNeedsStatusBarAppearanceUpdate()
-            setNeedsUpdateOfHomeIndicatorAutoHidden()
-            setNeedsUpdateOfScreenEdgesDeferringSystemGestures()
+            updateAppearance(animated: true)
+        }
+    }
+    
+    private func updateAppearance(animated: Bool) {
+        let animations = {
+            self.setNeedsStatusBarAppearanceUpdate()
+            self.setNeedsUpdateOfHomeIndicatorAutoHidden()
+            self.setNeedsUpdateOfScreenEdgesDeferringSystemGestures()
+            if #available(iOS 14.0, *) {
+                self.setNeedsUpdateOfPrefersPointerLocked()
+            }
+        }
+        
+        if animated {
+            UIView.animate(withDuration: 0.21, animations: animations)
+        } else {
+            animations()
         }
     }
 }
