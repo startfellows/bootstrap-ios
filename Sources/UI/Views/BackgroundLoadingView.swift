62ab8d02957dc95e7bde3c04611a98b4cea20ce3
Anton Spivak <oboupo@gmail.com>
2021-06-21 19:51:13 +0300

    Add text to background view

--- Sources/UI/Views/BackgroundLoadingView.swift
+++ Sources/UI/Views/BackgroundLoadingView.swift
@@ -6,10 +6,26 @@ import UIKit
 
 public class BackgroundLoadingView: UIView {
     
+    public enum State {
+        
+        case empty
+        case loading(loading: Bool)
+        case text(text: String)
+    }
+    
     let loadingIndicatorView = UIActivityIndicatorView(style: .large)
+    let label: UILabel = UILabel()
+    
+    public private(set) var state: State = .empty
     
     public override var backgroundColor: UIColor? { set {} get { .clear } }
-    public override var tintColor: UIColor! { set { loadingIndicatorView.color = newValue } get { loadingIndicatorView.color } }
+    public override var tintColor: UIColor! {
+        set {
+            loadingIndicatorView.color = newValue
+            label.textColor = newValue
+        }
+        get { loadingIndicatorView.color }
+    }
     
     public override init(frame: CGRect) {
         super.init(frame: frame)
@@ -23,20 +39,52 @@ public class BackgroundLoadingView: UIView {
     
     private func initialize() {
         super.backgroundColor = .clear
-        loadingIndicatorView.hidesWhenStopped = true
+        
+        label.font = .systemFont(ofSize: 17, weight: .regular)
+        loadingIndicatorView.hidesWhenStopped = false
+        
         addSubview(loadingIndicatorView)
+        addSubview(label)
+        
+        update(state)
     }
     
     public override func layoutSubviews() {
         super.layoutSubviews()
         loadingIndicatorView.center = CGPoint(x: bounds.midX, y: bounds.midY)
+        
+        label.sizeToFit()
+        label.center = CGPoint(x: bounds.midX, y: bounds.midY)
     }
     
-    public func startAnimating() {
-        loadingIndicatorView.startAnimating()
-    }
-    
-    public func stopAnimating() {
-        loadingIndicatorView.stopAnimating()
+    public func update(_ state: State) {
+        switch state {
+        case .empty:
+            loadingIndicatorView.stopAnimating()
+            loadingIndicatorView.alpha = 0
+            label.alpha = 0
+        case .loading(let loading):
+            if loading {
+                loadingIndicatorView.startAnimating()
+                loadingIndicatorView.alpha = 1
+            } else {
+                loadingIndicatorView.stopAnimating()
+                loadingIndicatorView.alpha = 0
+            }
+            label.alpha = 0
+        case .text(let text):
+            label.text = text
+            label.alpha = 1
+            
+            loadingIndicatorView.alpha = 0
+            loadingIndicatorView.stopAnimating()
+            
+            UIView.performWithoutAnimation({
+                setNeedsLayout()
+                layoutIfNeeded()
+            })
+        }
+        
+        self.state = state
     }
 }
