77888f121b2b7b2255d63d79c8f0a9b1ae455972
Anton <oboupo@gmail.com>
2021-06-30 19:52:48 +0300

    Add ability to set custom image

--- Sources/Modules/Loading/LoadingSFViewController/LoadingSFViewController.swift
+++ Sources/Modules/Loading/LoadingSFViewController/LoadingSFViewController.swift
@@ -7,13 +7,26 @@ import UIKit
 final public class LoadingSFViewController: LoadingViewController {
     
     private var loadingSFView: LoadingSFView { view as! LoadingSFView }
+    private var image: UIImage? = nil
     
     public convenience init() {
         self.init(nibName: "LoadingSFView", bundle: Bundle.module)
+        #if DEBUG
+        self.image = UIImage(named: "logo-debug-512", in: Bundle.module, with: nil)
+        #else
+        self.image = UIImage(named: "logo-release-512", in: Bundle.module, with: nil)
+        #endif
+    }
+    
+    public convenience init(image: UIImage?) {
+        self.init(nibName: "LoadingSFView", bundle: Bundle.module)
+        self.image = image
     }
     
     public override func prepare() {
         super.prepare()
+        
+        loadingSFView.imageView.image = image
         
         var transform = CATransform3DIdentity
         transform.m34 = 1.0 / -100
