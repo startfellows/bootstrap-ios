3df643ca81c8ba73889d2332dd30ac07695891a9
Anton <oboupo@gmail.com>
2021-07-08 15:35:45 +0300

    Add UserDefaults wrapper

---
+++ Sources/Utilites/Wrappers.swift
@@ -0,0 +1,32 @@
+//
+//  Created by Anton Spivak.
+//  
+
+import Foundation
+
+@propertyWrapper
+public struct UserDefaultsValue<T: Codable> {
+    
+    private let key: String
+    private let defaultValue: T
+
+    public init(key: String, defaultValue: T) {
+        self.key = key
+        self.defaultValue = defaultValue
+    }
+
+    public var wrappedValue: T {
+        get {
+            guard let data = UserDefaults.standard.object(forKey: key) as? Data else {
+                return defaultValue
+            }
+            
+            let value = try? JSONDecoder().decode(T.self, from: data)
+            return value ?? defaultValue
+        }
+        set {
+            let data = try? JSONEncoder().encode(newValue)
+            UserDefaults.standard.set(data, forKey: key)
+        }
+    }
+}
