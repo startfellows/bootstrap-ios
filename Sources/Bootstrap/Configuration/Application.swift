885440fd25c2348b28d3de3866c6a56b21238143
Anton Spivak <oboupo@gmail.com>
2021-07-31 21:23:26 +0300

    Tmp comment

--- Sources/Bootstrap/Configuration/Application.swift
+++ Sources/Bootstrap/Configuration/Application.swift
@@ -6,18 +6,18 @@ import UIKit
 
 final public class Application: UIApplication {
     
-    public class override var shared: Application {
-        typealias alias = @convention(c) (Application.Type, Selector) -> Application
-        let sel = NSSelectorFromString("sharedApplication")
-        
-        if (Bundle.main.executablePath?.contains(".appex/") ?? false) || !responds(to: sel) {
-            fatalError("Application.shared not available in Application Extension")
-        } else {
-            let imp = UIApplication.method(for: sel)
-            let function = unsafeBitCast(imp, to: alias.self)
-            return function(self, sel)
-        }
-    }
+//    public class override var shared: Application {
+//        typealias alias = @convention(c) (Application.Type, Selector) -> Application
+//        let sel = NSSelectorFromString("sharedApplication")
+//
+//        if (Bundle.main.executablePath?.contains(".appex/") ?? false) || !responds(to: sel) {
+//            fatalError("Application.shared not available in Application Extension")
+//        } else {
+//            let imp = UIApplication.method(for: sel)
+//            let function = unsafeBitCast(imp, to: alias.self)
+//            return function(self, sel)
+//        }
+//    }
     
     final var windowScenes: [WindowScene] { connectedScenes.compactMap({ $0 as? WindowScene }) }
     final var windowSceneForeground: WindowScene? { windowScenes.first(where: { $0.activationState == .foregroundActive }) }
